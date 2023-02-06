f9ef7e571b3ed1ea5997db33688dc2be86d8474a
bryanvullo <bryanvullo04@gmail.com>
2023-02-06 21:02:27 +0000

    Question 1 complete

---
+++ Lab-1/src/RandomNumberWriter.java
@@ -0,0 +1,44 @@
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.util.Random;
+import uk.ac.soton.ecs.comp1206.labtestlibrary.interfaces.io.RandomIO;
+
+public class RandomNumberWriter implements RandomIO {
+  Random random;
+  public RandomNumberWriter(long seed) {
+    random = new Random(seed);
+  }
+
+  @Override
+  public void writeRandomChars(String path) throws IOException {
+    File file = new File(path);
+    FileWriter writer = new FileWriter(file);
+    for (int i = 0; i < 10000; i++) {
+      int randomNumber = random.nextInt(100000);
+      writer.write(randomNumber + "\n");
+    }
+    writer.close();
+  }
+
+  @Override
+  public void writeRandomByte(String path) throws IOException {
+    File file = new File(path);
+    FileOutputStream fileOutputStream = new FileOutputStream(file);
+    ByteBuffer byteBuffer = ByteBuffer.allocate(4);
+
+    for (int i = 0; i < 10000; i++) {
+      int randomInteger = random.nextInt(100000);
+      byteBuffer.putInt(randomInteger);
+      fileOutputStream.write(byteBuffer.array());
+      byteBuffer.clear();
+    }
+    fileOutputStream.close();
+  }
+}
