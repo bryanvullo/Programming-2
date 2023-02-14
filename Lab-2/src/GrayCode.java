bfe7f39154c042d5a402ac5a823ec0628a6edc59
bryanvullo <bryanvullo04@gmail.com>
2023-02-14 11:17:38 +0000

    trying to make the gray code generator

---
+++ Lab-2/src/GrayCode.java
@@ -0,0 +1,17 @@
+public class GrayCode {
+
+  public static int[] grayCode(int n) {
+    int rows = (int) Math.pow(2, n);
+    int[][] arr = new int[n][rows];
+    if (n < 1) {
+      System.err.println("invalid input");
+    } else if (n == 1) {
+      return new int[] {0,1};
+    } else {
+      for (int i = 0; i < rows/2; i++) {
+        arr[i] = new int[]{0, grayCode(n - 1)};
+      }
+    }
+  }
+
+}
