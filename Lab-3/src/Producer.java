8cac9f50b7acada2ef30ee10d678f826272b2d68
bryanvullo <bryanvullo04@gmail.com>
2023-02-21 16:09:01 +0000

    Lab 3 Question 3 Complete

---
+++ Lab-3/src/Producer.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+import uk.ac.soton.ecs.comp1206.labtestlibrary.interfaces.threading.FactoryWorker;
+import uk.ac.soton.ecs.comp1206.labtestlibrary.interfaces.threading.NumberQueue;
+
+public class Producer extends FactoryWorker {
+  private Random random;
+
+  public Producer(int id, NumberQueue belt) {
+    super("Producer", id, belt);
+    random = new Random();
+  }
+
+  public void message(int number) {
+    System.out.println(this.jobType + " " + this.id + " added " + number + " to the belt.");
+  }
+
+  public int action() {
+    int number = random.nextInt(10000, 99999);
+    this.belt.enqueue(number);
+    return number;
+  }
+
+  public void run() {
+    while (!Thread.currentThread().isInterrupted()) {
+      try {
+        int number = action();
+        message(number);
+      } catch (Exception e) {
+        messageError();
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    CyclicQueue queue = new CyclicQueue(20);
+    Producer producer = new Producer(1, queue);
+    Consumer consumer = new Consumer(2,queue);
+
+    Thread pt = new Thread(producer, "producer");
+    Thread ct = new Thread(consumer, "consumer");
+
+    pt.start();
+    ct.start();
+  }
+}
